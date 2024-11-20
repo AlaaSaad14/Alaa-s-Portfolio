ad76340279d8e197b059b306e796d955a372988e
Alaa Saad <119546583+AlaaSaad14@users.noreply.github.com>
2024-11-20 23:22:41 +0200

    Responsive Styling

---
+++ Portfolio.js
@@ -0,0 +1,40 @@
+
+// var typed = new typed(".text", {
+//     String: ["FrontEnd Developer", "UI/UX Designer", "Graphic Designer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// }
+
+// ) 
+
+
+// <!----------------------------- about ----------------------------- -->
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname ).classList.add("active-tab")
+}
+
+// <!-----------------------------  menu ----------------------------- -->
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openMenu() {
+    sidemenu.style.right = "0";
+}
+function closeMenu() {
+    sidemenu.style.right = "-200px";
+}    
